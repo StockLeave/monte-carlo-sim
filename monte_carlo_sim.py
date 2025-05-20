e24ab57d91b01f5e688391bdb603499a51668175
StockLeave <stockleave@gmail.com>
2025-05-20 15:23:04 +0200

    Update monte_carlo_sim.py

--- monte_carlo_sim.py
+++ monte_carlo_sim.py
@@ -78,7 +78,6 @@ with col1:
     st.markdown("**Final Balance**")
     st.markdown(f"Average: ${avg_balance:,.2f}")
     st.markdown(f"Standard Deviation: ${std_dev:,.2f}")
-    st.markdown(f"Median: ${median_balance:,.2f}")
     st.markdown(f"Best Case: ${max_balance:,.2f}")
     st.markdown(f"Worst Case: ${min_balance:,.2f}")
 
